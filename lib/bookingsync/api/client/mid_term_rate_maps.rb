636020409a652f0b89f83ee3574a032d96a3d3e9
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2026-04-20 16:50:16 +0100

    Add show endpoint for mid term rate map

--- lib/bookingsync/api/client/mid_term_rate_maps.rb
+++ lib/bookingsync/api/client/mid_term_rate_maps.rb
@@ -4,6 +4,10 @@ module BookingSync::API
       def mid_term_rate_maps(options = {}, &block)
         paginate :mid_term_rate_maps, options, &block
       end
+
+      def mid_term_rate_map(mid_term_rate_map)
+        get("mid_term_rate_maps/#{mid_term_rate_map}").pop
+      end
     end
   end
 end
