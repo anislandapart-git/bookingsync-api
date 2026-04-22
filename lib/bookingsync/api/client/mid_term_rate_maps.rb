c14c1531fe2a87f7ee903f99bfe6e142bf6eccb5
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2026-04-22 16:57:14 +0100

    Allow for passing options[:id] to filter mid_term_rate_maps endpoint to respective mid term rate map id

--- lib/bookingsync/api/client/mid_term_rate_maps.rb
+++ lib/bookingsync/api/client/mid_term_rate_maps.rb
@@ -2,11 +2,13 @@ module BookingSync::API
   class Client
     module MidTermRateMaps
       def mid_term_rate_maps(options = {}, &block)
+        return mid_term_rate_map(options[:id]) if options[:id]
+
         paginate :mid_term_rate_maps, options, &block
       end
 
-      def mid_term_rate_map(options = {})
-        get("mid_term_rate_maps/#{options.delete('rental_id')}").pop
+      def mid_term_rate_map(mid_term_rate_map)
+        get("mid_term_rate_maps/#{mid_term_rate_map}").pop
       end
     end
   end
--- lib/bookingsync/api/version.rb
+++ lib/bookingsync/api/version.rb
@@ -1,5 +1,5 @@
 module BookingSync
   module API
-    VERSION = '1.2.6-aia'
+    VERSION = '1.2.7-aia'
   end
 end
